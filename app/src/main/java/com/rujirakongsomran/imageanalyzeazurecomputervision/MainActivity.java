e9b86337827aa5a1deae348a627c5c3fd6349304
Rujira Kongsomran <biwberry@outlook.com>
2020-03-24 23:04:28 +0700

    Design UI

--- app/src/main/java/com/rujirakongsomran/imageanalyzeazurecomputervision/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/imageanalyzeazurecomputervision/MainActivity.java
@@ -3,12 +3,27 @@ package com.rujirakongsomran.imageanalyzeazurecomputervision;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button btnProcess;
+    TextView txtResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btnProcess = (Button) findViewById(R.id.btnProcess);
+        txtResult = (TextView) findViewById(R.id.txtResult);
     }
 }
